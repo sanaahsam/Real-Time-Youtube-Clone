e7bd752ba4765c59285bd82c805f76a43e9ae431
Sana Ahsam <129504033+sanaahsam@users.noreply.github.com>
2024-06-09 23:27:33 +0530

    updated

--- Server/server.js
+++ Server/server.js
@@ -19,26 +19,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "https://real-time-youtube-clone.vercel.app",
-    methods: ["GET", "POST", "PUT"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
-  },
-});
-
-app.use(
-  cors({
-    origin: "https://real-time-youtube-clone.vercel.app",
-    methods: ["GET", "POST", "PUT"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
-  })
-);
-
 app.use(express.json());
+app.use(cors());
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.use("/Youtube", VideoRoute);
@@ -57,13 +39,22 @@ const connect = async () => {
         useUnifiedTopology: true,
       }
     );
-
     console.log("connected to database");
   } catch (error) {
     console.error("Error connecting to the database", error);
     process.exit(1);
   }
 };
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "https://real-time-youtube-clone.vercel.app",
+    methods: ["GET", "POST", "PUT"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("a user connected");
@@ -73,9 +64,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
   connect();
-  console.log(`Server is listening on port ${process.env.PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
-
-export { io };
